ed641d107e1ec7f89d9480c520a28eff37340db7
gLitCH1904 <95301749+gLitCH1904@users.noreply.github.com>
2022-08-15 23:59:34 +0100

    Add files via upload

--- faq.js
+++ faq.js
@@ -1,7 +1,7 @@
 const popUp = document.querySelectorAll(".pop-up");
 const overlay = document.querySelector(".overlay");
 const arrow = document.querySelectorAll(".arrow");
-const row = document.querySelector(".row");
+const row = document.querySelectorAll(".row");
 
 // document.addEventListener("click", () => {
 //   for (let i = 0; i < overlay.length; i++) {
@@ -17,8 +17,13 @@ for (let i = 0; i < arrow.length; i++) {
         arrow[i].classList.toggle("active");
       }
     }
+
+    for (let e = 0; e < row.length; e++) {
+      if (i === e) {
+        row[e].classList.toggle("active");
+      }
+    }
     overlay.classList.toggle("active");
-    row.classList.toggle("active");
   });
 }
 
@@ -28,6 +33,10 @@ overlay.addEventListener("click", () => {
   }
   for (let b = 0; b < popUp.length; b++) {
     popUp[b].classList.remove("active");
+  }
+
+  for (let e = 0; e < row.length; e++) {
+    row[e].classList.remove("active");
   }
   overlay.classList.remove("active");
 });
